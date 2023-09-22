588c4e95d89c89d1ac6507e2859cd7299561af47
jic <ji_camargo@yahoo.com>
2023-09-21 20:39:20 -0500

    add precompile files

---
+++ public/assets/navbar-f4cd6f9966b8d5aa7376eea73f1d418df1657066df22cb0c6248cdaa305c22a0.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburgerButton = document.querySelector('.menu-btn-cnt');
+  const menuDropdown = document.querySelector('.navbar-menu');
+  const overlay = document.querySelector('.overlay');
+  const closeButton = document.querySelector('.menu-close');
+
+   // Add a click event to the hamburger button
+  hamburgerButton.addEventListener('click', function() {
+    // Toggle the "active" class on both the hamburger button and the dropdown menu 
+    hamburgerButton.classList.toggle('btn-on');
+    menuDropdown.classList.toggle('active');
+    overlay.classList.toggle('active');
+  });
+  
+  //add click event to the close button
+  closeButton.addEventListener('click', function() {
+    // Toggle the "active" class on both the hamburger button and the dropdown menu
+    hamburgerButton.classList.toggle('btn-on');
+    menuDropdown.classList.toggle('active');
+    overlay.classList.toggle('active');
+  });
+
+  //add click event to the overlay
+  overlay.addEventListener('click', function() {
+    // Toggle the "active" class on both the hamburger button and the dropdown menu
+    hamburgerButton.classList.toggle('btn-on');
+    menuDropdown.classList.toggle('active');
+    overlay.classList.toggle('active');
+  });
+});
