57c80224ca7a5ede145f705c2e38d01f55da43ab
jic <ji_camargo@yahoo.com>
2023-09-21 21:07:06 -0500

    load precompiled assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,9 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( application.css )
+Rails.application.config.assets.precompile += %w( general_styles.css )
+Rails.application.config.assets.precompile += %w( payments.css )
+Rails.application.config.assets.precompile += %w( groups.js )
+Rails.application.config.assets.precompile += %w( navbar.js )
