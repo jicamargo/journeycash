d3270aaf4841c2a5f6944ae312fb3fb0805563c2
jic <ji_camargo@yahoo.com>
2023-11-29 08:34:01 -0500

    relocate JS files (navbar and groups) in app/javascript folder

--- app/javascript/general/navbar.js
+++ app/javascript/general/navbar.js
@@ -1,10 +1,14 @@
+// save a local storage message for debugging
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
   const hamburgerButton = document.querySelector('.menu-btn-cnt');
   const menuDropdown = document.querySelector('.navbar-menu');
   const overlay = document.querySelector('.overlay');
   const closeButton = document.querySelector('.menu-close');
-
-   // Add a click event to the hamburger button
+  
+  // Add a click event to the hamburger button
   hamburgerButton.addEventListener('click', function() {
     // Toggle the "active" class on both the hamburger button and the dropdown menu 
     hamburgerButton.classList.toggle('btn-on');
